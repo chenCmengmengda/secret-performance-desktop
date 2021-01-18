02972960cdafaf3204642fd5f83f6b9bfaac1681
杨朝晖 <1481883582@qq.com>
2021-01-18 21:38:39 +0800

    优化文件是否存在代码！

--- src/main/java/cn/chenc/performs/util/FileUtil.java
+++ src/main/java/cn/chenc/performs/util/FileUtil.java
@@ -10,12 +10,14 @@ import java.io.File;
  */
 public class FileUtil {
 
-    public static boolean exists(String filePath){
-        File file=new File(filePath);
-        if(!file.exists()){
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * 判断该文件路径  文件或目录是否存在
+	 * @param filePath  文件路径
+	 * @return  存在返回true  不存在返回false
+	 */
+	public static boolean exists(String filePath){
+		if(new File(filePath).exists()) return true;
+		return false;
+	}
 
 }
